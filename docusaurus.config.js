f5e01fcbd769d375a41fb14a6f858b55ebcc1403
igoogolx <igoogolx@gmail.com>
2023-09-19 09:45:13 +0800

    refactor: remove unused code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,23 +97,6 @@ const config = {
             ],
           },
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
             title: 'More',
             items: [
               {
@@ -127,7 +110,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Lux, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
