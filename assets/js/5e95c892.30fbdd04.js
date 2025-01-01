40653717f964f825d5e27bed3ca5e12c3cc2ff01
igoogolx <27353191+igoogolx@users.noreply.github.com>
2025-01-01 09:34:40 +0000

    Deploying to gh-pages from @ igoogolx/lux-docs@228a0ba81e05f58b3af7ad53fb8a009aa1544d5f 🚀

---
+++ assets/js/5e95c892.30fbdd04.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklux_docs=self.webpackChunklux_docs||[]).push([[647],{3713:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var u=r(4164),c=r(1308),a=r(9872),l=r(2831),d=r(1771),n=r(4848);function t(e){return(0,n.jsx)(c.e3,{className:(0,u.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,l.v)(e.route.routes)})})}}}]);
